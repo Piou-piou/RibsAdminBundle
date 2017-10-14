7b205980c26f60691928a7cfca3f60e99e07b33c
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-10-14 17:34:07 +0200

    [edit] change name of bundle

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('ribs_framework');
-
+        $rootNode = $treeBuilder->root('ribs_admin');
+		
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
