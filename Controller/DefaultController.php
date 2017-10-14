5cbd2cb78c403488468a14413cf15b86c95bef72
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-10-14 18:55:51 +0200

    [edit] simplify index route

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,7 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/ribs-index", name="ribsadmin_index")
+     * @Route("/index", name="ribsadmin_index")
      */
     public function indexAction()
     {
