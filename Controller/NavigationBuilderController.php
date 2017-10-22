a2ff31fb98239f99e6799c23521b4924ad25e807
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-10-22 12:37:48 +0200

    [func] prepare display left navigation

---
+++ Controller/NavigationBuilderController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Ribs\RibsAdminBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class NavigationBuilderController extends Controller
+{
+	public function getLeftNavigationAction()
+	{
+		$navigation = json_decode(file_get_contents($this->em->get('kernel')->getRootDir() . "/../src/Ribs/RibsAdminBundle/Resources/json/navigation.json"));
+		$menu = null;
+		
+		foreach ($navigation["items"] as $item) {
+			if ($this->get("ribs_admin.acess_rights")->testRight($item["right"])) {
+				$menu = [
+					"url" => $item["url"]
+				];
+			}
+		}
+		
+		dump($menu);
+		
+		return new Response();
+		
+		return $menu;
+	}
+}
