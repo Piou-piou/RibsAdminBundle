5f2b4a8657eaefe6c18dbf38d055a827941fd906
Piou-piou <pilloud.anthony@gmail.com>
2017-11-01 15:45:07 +0100

    [func] prepare babel for es6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,9 @@
-$(document).ready(function() {
-    $(document).ready(function() {
-        $(".menu .title i").click(function() {
-            $(this).parent().parent().toggleClass("active");
-        });
-        
-        $(".menu ul li").click(function() {
-            window.location = $(this).find("a").attr("href");
-        });
-    })
+$(document).ready(function () {
+	$(".menu .title i").click(function () {
+		$(this).parent().parent().toggleClass("active");
+	});
+	
+	$(".menu ul li").click(function () {
+		window.location = $(this).find("a").attr("href");
+	});
 });
