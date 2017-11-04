413379da00a282a7ae4547304b78fb863832329d
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-11-04 22:27:05 +0100

    [edit] add method to reactivate an account

--- Controller/AccountsController.php
+++ Controller/AccountsController.php
@@ -12,39 +12,48 @@ class AccountsController extends Controller
 	//-------------------------------------------- DISPLAY VIEWS ---------------------------------------------------------------//
 	/**
 	 * @Route("/accounts", name="ribsadmin_accounts")
-	 * @return Response function that return a list of all users that are not archived and different of current accout
+	 * @return Response function that return a list of all users that are archived or not and different of current accout
 	 */
 	public function AccountsListAction(): Response
 	{
 		$em = $this->getDoctrine()->getManager();
 		$current_account = $this->getUser()->getUser();
 		
-		$users = $em->getRepository("RibsAdminBundle:FosUser")->findAllUserNoArchived($current_account);
+		$users = $em->getRepository("RibsAdminBundle:FosUser")->findAllUserArchived($current_account);
+		$users_archived = $em->getRepository("RibsAdminBundle:FosUser")->findAllUserArchived($current_account, true);
 		
 		return $this->render('@RibsAdmin/accounts/list-all-accounts.html.twig', [
-			"users" => $users
+			"users" => $users,
+			"users_archived" => $users_archived
 		]);
 	}
 	//-------------------------------------------- END DISPLAY VIEWS -----------------------------------------------------------//
 	
 	/**
-	 * @Route("/account/archive/{guid}", name="ribsadmin_account_archive")
+	 * @Route("/account/archive/{guid}/{activate}", name="ribsadmin_account_archive")
 	 * @param string $guid
-	 * @return RedirectResponse function that will archive a user
+	 * @return RedirectResponse function that will disable or activate a user
 	 */
-	public function archiveAccount(string $guid): RedirectResponse
+	public function archiveAccount(string $guid, bool $activate = false): RedirectResponse
 	{
 		$em = $this->getDoctrine()->getManager();
 		
 		$user = $em->getRepository("RibsAdminBundle:User")->findOneBy(["guid" => $guid]);
 		
 		if ($user) {
-			$user->setArchived(true);
+			if ($activate === true) {
+				$user->setArchived(false);
+				$word = "activated";
+			} else {
+				$user->setArchived(true);
+				$word = "disabled";
+			}
+			
 			$em->persist($user);
 			$em->flush();
 			
 			$this->addFlash("success-flash", "The user " . $user->getFirstname() . " " . $user->getLastname() .
-				" was archived sucessfuly");
+				" was " . $word . " sucessfuly");
 		}
 		
 		return $this->redirectToRoute("ribsadmin_accounts");
--- Repository/FosUserRepository.php
+++ Repository/FosUserRepository.php
@@ -9,14 +9,17 @@ class FosUserRepository extends EntityRepository
 {
 	/**
 	 * @param User $current_account
+	 * @param bool $archived
 	 * @return array function that return a list of all users that are not archived and different of current accout
 	 */
-	public function findAllUserNoArchived(User $current_account): array
+	public function findAllUserArchived(User $current_account, bool $archived = false): array
 	{
 		$query = $this->getEntityManager()->createQuery("SELECT fu FROM RibsAdminBundle:FosUser fu
 			  JOIN RibsAdminBundle:User u  WITH fu.user = u
-			  WHERE u.archived = false and u != :current_account
-		")->setParameter("current_account", $current_account);
+			  WHERE u.archived = :archived and u != :current_account
+			")
+			->setParameter("archived", $archived)
+			->setParameter("current_account", $current_account);
 		
 		return $query->getResult();
 	}
