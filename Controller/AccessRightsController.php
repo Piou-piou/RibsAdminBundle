3f8c66b764ca21702019f86dc53ed9edb9b74b00
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-11-05 11:55:04 +0100

    [func] prepare access right management

---
+++ Controller/AccessRightsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ribs\RibsAdminBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AccessRightsController extends Controller
+{
+	/**
+	 * @return Response
+	 */
+	public function listAction(): Response {
+		return $this->render("@RibsAdmin/access-rights/list-all-list.html.twig");
+	}
+}
