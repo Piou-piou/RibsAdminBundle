1820c0d7065560b1e0f17a92134a17a6951cbdc8
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-11-11 21:27:34 +0100

    [func] add vendors for css

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ Encore
 	.cleanupOutputBeforeBuild()
 	
 	.addEntry("js/vendor", [
-		"./node_modules/jquery/dist/jquery.min.js"
+		"./node_modules/jquery/dist/jquery.min.js",
+        './assets/node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 	])
 	
 	.addEntry("js/main", [
@@ -16,14 +17,19 @@ Encore
 		"./assets/js/form_blocks/inputs.js",
 		"./assets/js/popups/flash-messages.js"
 	])
+
+    .addEntry("js/login", [
+        "./assets/js/login/particles.js",
+        "./assets/js/login/app.js"
+    ])
+
+    .addStyleEntry("css/vendor", [
+        './assets/node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
+    ])
 	
 	.addStyleEntry("css/style", [
 		"./assets/scss/style.scss"
-	])
-	
-	.addEntry("js/login", [
-		"./assets/js/login/particles.js",
-		"./assets/js/login/app.js"
+
 	])
 	
 	.enableSassLoader()
