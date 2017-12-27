a080870d822488ab00a4976a0e44f950043f6dc0
Anthony Pilloud <a.pilloud@onlineformapro.com>
2017-12-27 12:44:30 +0100

    [refa] var names in LoginController

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -16,60 +16,24 @@ class LoginController extends Controller
 	 *
 	 * @return Response
 	 */
-	public function loginAction(Request $request)
+	public function loginAction()
 	{
-		/** @var $session \Symfony\Component\HttpFoundation\Session\Session */
-		/*$session = $request->getSession();
-		
-		$authErrorKey = Security::AUTHENTICATION_ERROR;
-		$lastUsernameKey = Security::LAST_USERNAME;
-		$error = null;
-		
-		// get the error if any (works with forward and redirect -- see below)
-		if ($request->attributes->has($authErrorKey)) {
-			$error = $request->attributes->get($authErrorKey);
-		} else if (null !== $session && $session->has($authErrorKey)) {
-			$error = $session->get($authErrorKey);
-			$session->remove($authErrorKey);
-		}
-		
-		if (!$error instanceof AuthenticationException) {
-			$error = null; // The value does not come from the security component.
-		}
-		
-		if ($error !== null) {
-			$this->addFlash("error-flash", "Login informations are invalid");
-		}
-		
-		// last username entered by the user
-		$lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
-		
-		$csrfToken = $this->has('security.csrf.token_manager')
+		$csrf_token = $this->has('security.csrf.token_manager')
 			? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
 			: null;
 		
-		return $this->renderLogin([
-			'last_username' => $lastUsername,
-			'error' => $error,
-			'csrf_token' => $csrfToken,
-		]);*/
-		
-		$csrfToken = $this->has('security.csrf.token_manager')
-			? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue()
-			: null;
-		
-		$authUtils = $this->get("security.authentication_utils");
+		$auth_utils = $this->get("security.authentication_utils");
 		
 		// get the login error if there is one
-		$error = $authUtils->getLastAuthenticationError();
+		$error = $auth_utils->getLastAuthenticationError();
 		
 		// last username entered by the user
-		$lastUsername = $authUtils->getLastUsername();
+		$last_username = $auth_utils->getLastUsername();
 		
 		return $this->render('@RibsAdmin/login/login.html.twig', array(
-			'last_username' => $lastUsername,
-			'error'         => $error,
-			'csrf_token' => $csrfToken,
+			'last_username' => $last_username,
+			'error' => $error,
+			'csrf_token' => $csrf_token,
 		));
 	}
 	
