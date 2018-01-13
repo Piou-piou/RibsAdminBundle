3519d2152ab83e45ba205b6386f385490dd5af36
Piou-piou <pilloud.anthony@gmail.com>
2018-01-13 22:12:56 +0100

    [func] add page render

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -5,14 +5,21 @@ namespace Ribs\RibsAdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PageController extends Controller
 {
 	/**
-	 * @Route("/page/{url}", name="ribsadmin_navigation", requirements={"url" = "[a-zA-Z0-9\-\_\/]*"})
+	 * @Route("/page/{url}", name="page", requirements={"url" = "[a-zA-Z0-9\-\_\/]*"})
 	 */
 	public function renderPageAction(string $url): Response
 	{
-		return new Response();
+		$page = $this->getDoctrine()->getManager()->getRepository("RibsAdminBundle:Page")->findOneBy(["url" => $url]);
+		
+		if ($page) {
+			return $this->render("@RibsAdmin/page.html.twig", ["page" => $page]);
+		}
+		
+		throw new NotFoundHttpException("The required page does not exist");
 	}
 }
