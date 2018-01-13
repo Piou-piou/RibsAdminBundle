303f27f4b47d24cc3fbca5107369fa36b40b0542
Piou-piou <pilloud.anthony@gmail.com>
2018-01-13 10:34:22 +0100

    [func] prepare PageController

---
+++ Controller/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ribs\RibsAdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class PageController extends Controller
+{
+	/**
+	 * @Route("/page/{page}", name="ribsadmin_navigation")
+	 */
+	public function renderPageAction() {
+	
+	}
+}
