3308275d0f915992963a420ac5ef5af060bf934e
Piou-piou <pilloud.anthony@gmail.com>
2018-01-14 18:17:03 +0100

    [func] add method to get all navigation links of pages and modules

--- Repository/NavigationRepository.php
+++ Repository/NavigationRepository.php
@@ -3,15 +3,31 @@
 namespace Ribs\RibsAdminBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Ribs\RibsAdminBundle\Entity\AccessRight;
 
 class NavigationRepository extends EntityRepository
 {
 	/**
-	 * @param AccessRight $access_right
-	 * function that delete all user which are in a list of rights
+	 * @return array
+	 * function that return all navigation links of pages and modules
 	 */
-	public function findAllNavigationPage()
+	public function findAllNavigation(): array
+	{
+		$query = $this->getEntityManager()->getConnection()->prepare("SELECT p.url, p.title, p.title_tag FROM navigation n
+			INNER JOIN page p ON n.id_page = p.id AND p.displayed = 1
+			INNER JOIN module m ON n.id_module = m.id AND m.displayed = 1
+		  	ORDER BY n.order ASC
+ 		");
+		
+		$query->execute();
+		
+		return $query->fetchAll(\PDO::FETCH_ASSOC);
+	}
+	
+	/**
+	 * @return array
+	 * function that return all navigation links of pages
+	 */
+	public function findAllNavigationPage(): array
 	{
 		$query = $this->getEntityManager()->getConnection()->prepare("SELECT p.url, p.title, p.title_tag FROM navigation n
 			INNER JOIN page p ON n.id_page = p.id AND p.displayed = 1
