e109f0d264b49b08f716d2050a9572103cb374df
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-01-28 14:50:06 +0100

    [edit] route in DefaultCOntroller

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -13,6 +13,6 @@ class DefaultController extends Controller
 	 */
 	public function indexAction()
 	{
-		return $this->render('RibsAdminBundle:Default:index.html.twig');
+		return $this->render('@RibsAdmin/Default/index.html.twig');
 	}
 }
