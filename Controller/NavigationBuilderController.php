45d0a96be1913a104f6b9b16af72f9f806e9406e
Piou-piou <pilloud.anthony@gmail.com>
2018-01-29 21:16:08 +0100

    [func] beginning of getting modules navigation in admin

--- Controller/NavigationBuilderController.php
+++ Controller/NavigationBuilderController.php
@@ -2,25 +2,50 @@
 
 namespace PiouPiou\RibsAdminBundle\Controller;
 
+use PiouPiou\RibsAdminBundle\Entity\Module;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class NavigationBuilderController extends Controller
 {
+	private  $nav = [];
+	
 	/**
 	 * @return Response function that display the left navigation mapped by user rights
 	 */
 	public function getLeftNavigationAction(): Response
 	{
 		$navigation = json_decode(file_get_contents($this->get("ribs_admin.globals")->getBaseBundlePath() . "/Resources/json/navigation.json"), true);
-		$menu = [];
 		
 		foreach ($navigation["items"] as $item) {
 			if ($this->get("ribs_admin.acess_rights")->testRight($item["right"])) {
-				$menu[] = $item;
+				dump($item);
+				$this->nav[] = $item;
 			}
 		}
 		
-		return $this->render("@RibsAdmin/navigation.html.twig", ["navigation" => $menu]);
+		$this->getModuleNavigation();
+		
+		return $this->render("@RibsAdmin/navigation.html.twig", ["navigation" => $this->nav]);
+	}
+	
+	/**
+	 * to get all modules navigation and test right navigation
+	 */
+	private function getModuleNavigation()
+	{
+		$modules = $this->getDoctrine()->getRepository(Module::class)->findBy([
+			"active" => true,
+			"displayed" => true
+		]);
+		
+		foreach ($modules as $module) {
+			$this->nav[] = [
+				"right" => "ribsadmin@blog",
+				"url" => "ribsbadmin_index",
+				"icon" => "",
+				"text" => $module->getTitle()
+			];
+		}
 	}
 }
