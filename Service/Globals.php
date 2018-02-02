67fc7585f025a5a1a0e1d3755bc112a2c64cb2c3
Piou-piou <pilloud.anthony@gmail.com>
2018-02-02 13:04:35 +0100

    [func] add paring access right for other ribs packages

--- Service/Globals.php
+++ Service/Globals.php
@@ -5,13 +5,14 @@ namespace PiouPiou\RibsAdminBundle\Service;
 class Globals
 {
 	/**
+	 * @param string|null $package
 	 * @return string
 	 */
-	public function getBaseBundlePath(): string
+	public function getBaseBundlePath(string $package = "piou-piou/ribs-admin-bundle"): string
 	{
 		$path = explode("/", __DIR__);
 		array_pop($path);
 		
-		return implode("/", $path);
+		return implode("/", $path)."/../../" . $package;
 	}
 }
