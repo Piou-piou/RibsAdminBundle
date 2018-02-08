82086d47841ef26bd3bcc8bde71d6ef005dd1a91
Piou-piou <pilloud.anthony@gmail.com>
2018-02-08 20:30:37 +0100

    [refa] delete use useless

--- Form/AccessRight.php
+++ Form/AccessRight.php
@@ -2,7 +2,6 @@
 
 namespace PiouPiou\RibsAdminBundle\Form;
 
-use AppBundle\Entity\Profil;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
