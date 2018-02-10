ba94a9df546e1b1d01885e9f7f968dd7afa041a3
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-02-10 20:48:50 +0100

    [edit] edit form input and textarea view

--- assets/js/form_blocks/inputs.js
+++ assets/js/form_blocks/inputs.js
@@ -1,84 +1,88 @@
-$(document).ready(function() {
-	function ValidateDate(dtValue)  {
-		var dtRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
-		return dtRegex.test(dtValue);
-	}
+$(document).ready(function () {
+  function ValidateDate(dtValue) {
+    var dtRegex = new RegExp(/\b\d{1,2}[\/-]\d{1,2}[\/-]\d{4}\b/);
+    return dtRegex.test(dtValue);
+  }
 
-	//---------------------------- EFFECT MATERIAL ON INPUT INPUT -----------------------------//
-	$('.block input').focus(function() {
-		let $parent = $(this).parent();
-		$parent.addClass('is-focused has-label');
-		$parent.removeClass('invalid');
-	});
-
-	$('.block input').blur(function() {
+  //---------------------------- EFFECT MATERIAL ON INPUT INPUT -----------------------------//
+  $('.block input').focus(function () {
     let $parent = $(this).parent();
-		let $this = $(this);
+    $parent.addClass('is-focused has-label');
+    $parent.removeClass('invalid');
+  });
 
-        var type = $this.attr("type-val");
-        var min = $this.attr("min");
-        var max = $this.attr("max");
+  $('.block input').blur(function () {
+    let $parent = $(this).parent();
+    let $this = $(this);
 
-        if (type == "string") {
-            if ($this.val() == "") {
-                $parent.addClass('invalid');
-                $parent.removeClass('has-label');
-            }
-            else if (($this.val().length < min) || ($this.val().length > max)) {
-                $parent.addClass('invalid');
-            }
-        }
-        else if (type == "date") {
-            if (($this.attr('name') == "date") && (!ValidateDate($this.val()))) {
-                $parent.addClass('invalid');
-            }
-        }
+    const type = $this.attr("type-val");
+    const min = $this.attr("min");
+    const max = $this.attr("max");
 
-		$parent.removeClass('is-focused');
+    if (type == "string") {
+      if ($this.val() == "") {
+        $parent.addClass('invalid');
+        $parent.removeClass('has-label');
+      }
+      else if (($this.val().length < min) || ($this.val().length > max)) {
+        $parent.addClass('invalid');
+      }
+    }
+    else if (type == "date") {
+      if (($this.attr('name') == "date") && (!ValidateDate($this.val()))) {
+        $parent.addClass('invalid');
+      }
+    }
+
+    $parent.removeClass('is-focused');
 
     if ($this.val() == "") {
       $parent.removeClass('has-label');
     }
-	});
+  });
 
-	$('.block input').each(function() {
-		if (($(this).val() != '') || ($(this).val() != 0)) {
-			$(this).parent().addClass('has-label');
-		}
-	});
+  $('.block input').each(function () {
+    if (($(this).val() != '') || ($(this).val() != 0)) {
+      $(this).parent().addClass('has-label');
+    }
+  });
 
 
-	$('.block textarea').focus(function() {
-		$parent = $(this).parent();
-		$parent.addClass('is-focused has-label');
-		$parent.removeClass('invalid');
-	});
+  $('.block textarea').focus(function () {
+    let $parent = $(this).parent();
+    $parent.addClass('is-focused has-label');
+    $parent.removeClass('invalid');
+  });
 
-	$('.block textarea').blur(function() {
-		$parent = $(this).parent();
-		$this = $(this);
+  $('.block textarea').blur(function () {
+    let $parent = $(this).parent();
+    let $this = $(this);
 
-        var type = $this.attr("type-val");
-        var min = $this.attr("min");
-        var max = $this.attr("max");
+    const type = $this.attr("type-val");
+    const min = $this.attr("min");
+    const max = $this.attr("max");
 
-        if (type == "string") {
-            if ($this.val() == "") {
-                $parent.addClass('invalid');
-                $parent.removeClass('has-label');
-            }
-            else if (($this.val().length < min) || ($this.val().length > max)) {
-                $parent.addClass('invalid');
-            }
-        }
+    if (type == "string") {
+      if ($this.val() == "") {
+        $parent.addClass('invalid');
+        $parent.removeClass('has-label');
+      }
+      else if (($this.val().length < min) || ($this.val().length > max)) {
+        $parent.addClass('invalid');
+      }
+    }
 
-		$parent.removeClass('is-focused');
-	});
+    $parent.removeClass('is-focused');
 
-	$('.block textarea').each(function() {
-		if (($(this).val() != '') || ($(this).val() != 0)) {
-			$(this).parent().addClass('has-label');
-		}
-	});
-    //---------------------------- END EFFECT MATERIAL ON INPUT INPUT -----------------------------//
-})
+    if ($this.val() == "") {
+      $parent.removeClass('has-label');
+    }
+  });
+
+  $('.block textarea').each(function () {
+    if (($(this).val() != '') || ($(this).val() != 0)) {
+      $(this).parent().addClass('has-label');
+    }
+  });
+  //---------------------------- END EFFECT MATERIAL ON INPUT INPUT -----------------------------//
+});
