d2a0e900300cc1ce84c8565a64246e2bf24383d3
Piou-piou <pilloud.anthony@gmail.com>
2018-02-21 21:23:57 +0100

    [del] delete fancybox

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,25 +11,4 @@ $(document).ready(() => {
     $(".menu ul li").click(function () {
         window.location = $(this).find("a").attr("href");
     });
-
-    $('[data-fancybox]').fancybox({
-        smallBtn: false,
-        toolbar: false,
-        touch: false,
-        beforeShow(instance) {
-            const message = instance.$lastFocus[0].getAttribute('data-message');
-            const href = instance.$lastFocus[0].getAttribute('data-href');
-
-            if (message !== undefined) {
-                $('#info-popup p').html(message);
-            }
-
-            $('#info-popup a.validate').attr('href', href);
-        },
-        afterClose() {
-            // pour click sur un bouton qui ouvre #info-popup
-            $('#info-popup p').html('');
-            $('input#param').val('');
-        },
-    });
 });
