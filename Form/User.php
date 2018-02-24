576b00f7be687bb3185f99c157ac9d5f7cbd03c9
Piou-piou <pilloud.anthony@gmail.com>
2018-02-24 20:37:05 +0100

    [func] add form to create user (wip)

--- Form/User.php
+++ Form/User.php
@@ -3,6 +3,7 @@
 namespace PiouPiou\RibsAdminBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
