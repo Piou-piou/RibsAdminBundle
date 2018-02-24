676d67befabb5c5715a9e24a7a3774e9f44405a1
Piou-piou <pilloud.anthony@gmail.com>
2018-02-24 11:33:49 +0100

    [fix] bug js in login when .menu doesn't find in page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,20 @@
 import RibsCore from 'ribs-core';
 
-document.querySelector('.menu .title i').addEventListener('click', (event) => {
-  const inners = document.querySelectorAll('.inner');
-  RibsCore.parents(event.currentTarget, '.menu').classList.toggle('active');
-  document.querySelector('.nav-page').classList.toggle('displayed-nav');
+if (document.querySelector('.menu .title i')) {
+  document.querySelector('.menu .title i').addEventListener('click', (event) => {
+    const inners = document.querySelectorAll('.inner');
+    RibsCore.parents(event.currentTarget, '.menu').classList.toggle('active');
+    document.querySelector('.nav-page').classList.toggle('displayed-nav');
 
-  Array.from(inners).forEach((element) => {
-    element.classList.toggle('displayed-nav')
+    Array.from(inners).forEach((element) => {
+      element.classList.toggle('displayed-nav')
+    });
   });
-});
 
-const lis = document.querySelectorAll('.menu ul li');
-Array.from(lis).forEach((element) => {
-  element.addEventListener('click', (event) => {
-    window.location = event.currentTarget.querySelector('a').href;
+  const lis = document.querySelectorAll('.menu ul li');
+  Array.from(lis).forEach((element) => {
+    element.addEventListener('click', (event) => {
+      window.location = event.currentTarget.querySelector('a').href;
+    });
   });
-});
+}
