c42c10be4a15566f032f5ace293b60b71d8a6519
Piou-piou <pilloud.anthony@gmail.com>
2018-11-07 20:54:35 +0100

    [edit] add all api page outside of rights

--- Service/AccessRights.php
+++ Service/AccessRights.php
@@ -68,7 +68,9 @@ class AccessRights
 	public function onKernelController()
 	{
 		$route = $this->request->getCurrentRequest()->get("_route");
-		$admin_page = explode("_", $route)[0];
+		$route_array = explode("_", $route);
+		$admin_page = $route_array[0];
+		$api = count($route_array) > 1 ? $route_array[1] : null;
 		
 		//to show admin panel
 		if (in_array($route, ["_profiler", "_profiler_search_bar", "_wdt"])) {
@@ -79,7 +81,7 @@ class AccessRights
 		$modules_rights = $this->module->getModuleRights();
 		$ribs_admin_rights = (object)array_merge((array)$ribs_admin_rights, (array)$modules_rights);
 		
-		if ($admin_page == "ribsadmin" && strpos($route, "login") === false && strpos($route, "register") === false) {
+		if ($admin_page == "ribsadmin" && $api !== "api" && strpos($route, "login") === false && strpos($route, "register") === false) {
 			//redirection si user pas connecté
 			if ($this->container->get("security.token_storage")->getToken() === null || !$this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
 				return new RedirectResponse($this->router->generate("login"));
