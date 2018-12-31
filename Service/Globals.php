3821a944a1785eadfb5937ae7c335c68ffd7ed94
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-12-31 14:18:58 +0100

    [func] add dev mod in ribs to get correctly baseBundlePath in dev mode

--- Service/Globals.php
+++ Service/Globals.php
@@ -2,9 +2,40 @@
 
 namespace PiouPiou\RibsAdminBundle\Service;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class Globals
 {
 	/**
+	 * @var ContainerInterface
+	 */
+	private $container;
+	
+	public function __construct(ContainerInterface $container)
+	{
+		$this->container = $container;
+	}
+	
+	/**
+	 * this method send path on dev mode to correct write package name
+	 * @param string $package
+	 * @return string
+	 */
+	private function getPackageDevName(string $package) {
+		$bundle = explode("/", $package)[1];
+		$explode = explode("-", $bundle);
+		
+		$package_name = "";
+		
+		foreach ($explode as $string) {
+			$package_name .= ucfirst($string);
+		}
+		
+		return $package_name;
+	}
+	
+	/**
+	 * this method send base bundle path related to ribs-admin
 	 * @param string|null $package
 	 * @return string
 	 */
@@ -12,6 +43,12 @@ class Globals
 	{
 		$path = explode("/", __DIR__);
 		array_pop($path);
+		
+		$dev_mode = $this->container->getParameter("ribs_admin")["dev_mode"];
+		
+		if ($dev_mode === true) {
+			$package = "lib/".$this->getPackageDevName($package);
+		}
 		
 		return implode("/", $path)."/../../" . $package;
 	}
