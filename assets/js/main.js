733cedec88c3789ff0ea9b827afa76ee4d8b2c77
Piou-piou <pilloud.anthony@hmail.com>
2019-12-27 14:09:33 +0100

    [edit] pages visual

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 import RibsCore from 'ribs-core';
-import RibsAjax from 'ribs-admin-bundle-templates/source/js/RibsAjax';
+/*import RibsAjax from 'ribs-admin-bundle-templates/source/js/RibsAjax';*/
 import RibsFlashMessage from 'ribs-flash-message';
 import RibsPopup from "ribs-popup/source/js/ribs-popup";
 
-const ribsAjax = new RibsAjax('main', 'nav#left-nav', '#loader');
+/*const ribsAjax = new RibsAjax('main', 'nav#left-nav', '#loader');*/
 
 const flash = new RibsFlashMessage();
 const popups = new RibsPopup();
