70c5e3a40521059f90ae3474164e3ee20ab9fe08
Piou-piou <pilloud.anthony@gmail.com>
2020-01-03 09:25:24 +0100

    [func] prepare ribs-admin import module command

---
+++ Command/ImportModuleCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PiouPiou\RibsAdminBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ImportModuleCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('ribsadmin:import-module')
+            ->setDescription('Import a module in ribs admin')
+            /*->addArgument(
+                'name',
+                InputArgument::OPTIONAL,
+                'Who do you want to greet?'
+            )
+            ->addOption(
+                'yell',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the task will yell in uppercase letters'
+            )*/
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /*$name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }*/
+
+        $output->writeln("Houra !");
+    }
+}
