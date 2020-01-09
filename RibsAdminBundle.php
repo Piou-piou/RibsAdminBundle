7cb89af52fb47607e28405b8d378df1e32abea6d
Piou-piou <pilloud.anthony@gmail.com>
2020-01-09 09:08:45 +0100

    [func] add loader extension

--- RibsAdminBundle.php
+++ RibsAdminBundle.php
@@ -2,8 +2,13 @@
 
 namespace PiouPiou\RibsAdminBundle;
 
+use PiouPiou\RibsAdminBundle\DependencyInjection\RibsAdminExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class RibsAdminBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new RibsAdminExtension();
+    }
 }
