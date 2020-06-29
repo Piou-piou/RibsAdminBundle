7cf816d95da9a4effac615918e1648707ec7e91a
Piou-piou <pilloud.anthony@gmail.com>
2020-06-29 08:45:45 +0200

    [refa] Default controller indent

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,13 +7,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-	/**
-	 * @Route("/", name="ribsadmin")
-	 * @Route("/index", name="ribsadmin_index")
-	 * @Route("/navigation", name="ribsadmin_navigation")
-	 */
-	public function indexAction()
-	{
-		return $this->render('@RibsAdmin/Default/index.html.twig');
-	}
+    /**
+     * @Route("/", name="ribsadmin")
+     * @Route("/index", name="ribsadmin_index")
+     * @Route("/navigation", name="ribsadmin_navigation")
+     */
+    public function indexAction()
+    {
+        return $this->render('@RibsAdmin/Default/index.html.twig');
+    }
 }
