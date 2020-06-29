3922dae219c61016ecb8b17d171de33ae5ab91ec
Piou-piou <pilloud.anthony@gmail.com>
2020-06-29 08:50:23 +0200

    [refa] CreateUpdateAwareListener indent

--- EventListener/CreateUpdateAwareListener.php
+++ EventListener/CreateUpdateAwareListener.php
@@ -7,27 +7,27 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
 class CreateUpdateAwareListener
 {
-	/**
-	 * @var User
-	 */
-	private $user;
+    /**
+     * @var User
+     */
+    private $user;
 
     /**
      * CreateUpdateAwareListener constructor.
      * @param TokenStorage $tokenStorage
      */
     public function __construct(TokenStorage $tokenStorage)
-	{
+    {
         if ($tokenStorage->getToken() && is_object($tokenStorage->getToken()->getUser()) && $tokenStorage->getToken()->getUser()->getUser()) {
             $this->user = $tokenStorage->getToken()->getUser()->getUser();
         } else {
             $this->user = null;
         }
-	}
-	
-	public function prePersist($entity)
-	{
-	    if ($this->user) {
+    }
+
+    public function prePersist($entity)
+    {
+        if ($this->user) {
             if ($entity->getCreatedBy() === null) {
                 $entity->setCreatedAt(new \DateTime());
                 $entity->setCreatedBy($this->user);
@@ -37,7 +37,7 @@ class CreateUpdateAwareListener
                 $entity->setUpdatedBy($this->user);
             }
         }
-	}
+    }
 
     public function preUpdate($entity)
     {
