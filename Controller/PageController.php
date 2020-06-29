4b662f47104d7b2eb41be48be26926a785123a9a
Piou-piou <pilloud.anthony@gmail.com>
2020-06-29 08:48:30 +0200

    [refa] Page controller indent

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -9,14 +9,13 @@ use Symfony\Component\HttpFoundation\Response;
 class PageController extends AbstractController
 {
     /**
-     * method to show index page of content management
      * @Route("/contents", name="ribsadmin_contents")
      * @return Response
      */
-	public function indexAction(): Response
-	{
-		$navigation = $this->getDoctrine()->getManager()->getRepository("RibsAdminBundle:Navigation")->findAllNavigationPage();
-		
-		return $this->render('@RibsAdmin/page/index.html.twig', ["navigation" => $navigation]);
-	}
+    public function index(): Response
+    {
+        $navigation = $this->getDoctrine()->getManager()->getRepository("RibsAdminBundle:Navigation")->findAllNavigationPage();
+
+        return $this->render('@RibsAdmin/page/index.html.twig', ["navigation" => $navigation]);
+    }
 }
