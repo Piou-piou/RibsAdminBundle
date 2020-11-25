0b5fdda0b0fada1f2553df960e36421afdc3e532
Piou-piou <pilloud.anthony@gmail.com>
2020-11-25 08:22:28 +0100

    [func] prepare different routes for versions

--- Controller/VersionController.php
+++ Controller/VersionController.php
@@ -3,6 +3,7 @@
 namespace PiouPiou\RibsAdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,25 @@ class VersionController extends AbstractController
     public function index(): Response
     {
         return $this->render('@RibsAdmin/versions/list.html.twig');
+    }
+
+    /**
+     * @Route("/versions/edit/{id}", name="ribsadmin_versions_edit")
+     * @param int $id
+     * @return Response
+     */
+    public function edit(int $id): Response
+    {
+        return $this->render('@RibsAdmin/versions/edit.html.twig');
+    }
+
+    /**
+     * @Route("/versions/delete/{id}", name="ribsadmin_versions_delete")
+     * @param int $id
+     * @return RedirectResponse
+     */
+    public function delete(int $id): RedirectResponse
+    {
+        return $this->redirectToRoute("ribsadmin_modules");
     }
 }
