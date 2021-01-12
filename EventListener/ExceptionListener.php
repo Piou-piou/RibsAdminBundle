1539ded904f49b3512c171d5fb2d25e01e951d38
Piou-piou <pilloud.anthony@gmail.com>
2021-01-12 07:46:56 +0100

    [func] add method to retrieve well the user ip and disable logs if it is not the master request

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -5,6 +5,7 @@ namespace PiouPiou\RibsAdminBundle\EventListener;
 use PiouPiou\RibsAdminBundle\Entity\User;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class ExceptionListener
@@ -19,6 +20,11 @@ class ExceptionListener
      */
     private $user;
 
+    /**
+     * ExceptionListener constructor.
+     * @param ParameterBagInterface $parameterBag
+     * @param TokenStorageInterface $tokenStorage
+     */
     public function __construct(ParameterBagInterface $parameterBag, TokenStorageInterface $tokenStorage)
     {
         $this->paramter = $parameterBag;
@@ -27,17 +33,36 @@ class ExceptionListener
         }
     }
 
+    /**
+     * @return mixed
+     */
+    private function getClientIp()
+    {
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+
+        return $ip;
+    }
+
+    /**
+     * @param ExceptionEvent $event
+     */
     public function onKernelException(ExceptionEvent $event)
     {
         $slack_webhook = $this->paramter->get('ribs_admin.slack_webhook');
-        if ($slack_webhook) {
+        if ($slack_webhook && $event->getRequestType() === HttpKernelInterface::MASTER_REQUEST ) {
             $data = array();
             $data['channel'] = '#errors';
             $data['username'] = $_SERVER['HTTP_HOST'];
             $data['text'] = "• *Erreur* : " . strip_tags($event->getThrowable()->getMessage());
             $data['text'] .= "\n• *Erreur File* : " . strip_tags($event->getThrowable()->getFile()) . " at line :" . strip_tags($event->getThrowable()->getLine());
             $data['text'] .= "\n• *URL* : " . $event->getRequest()->getUri();
-            $data['text'] .= "\n• *IP* : " . $_SERVER["REMOTE_ADDR"];
+            $data['text'] .= "\n• *IP* : " . $this->getClientIp();
 
             if ($this->user) {
                 $data['text'] .= "\n• *Utilisateur* : " . $this->user . " with id : " . $this->user->getId();
@@ -49,7 +74,7 @@ class ExceptionListener
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $slack_webhook);
             curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data_json)));
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_exec($ch);
