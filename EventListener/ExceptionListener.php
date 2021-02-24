57dcabfe357c109d25259ad9e1c6497f1e8148cb
Piou-piou <pilloud.anthony@gmail.com>
2021-02-24 08:48:02 +0100

    [func] add automatic ip drop in firewall

--- EventListener/ExceptionListener.php
+++ EventListener/ExceptionListener.php
@@ -4,7 +4,9 @@ namespace PiouPiou\RibsAdminBundle\EventListener;
 
 use PiouPiou\RibsAdminBundle\Entity\User;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
@@ -56,6 +58,33 @@ class ExceptionListener
     {
         $slack_webhook = $this->paramter->get('ribs_admin.slack_webhook');
         if ($slack_webhook && $event->getRequestType() === HttpKernelInterface::MASTER_REQUEST ) {
+            $drop_txt = '';
+            if ($event->getThrowable() instanceof NotFoundHttpException) {
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, "https://ipinfo.io/".$this->getClientIp());
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $datas = curl_exec($ch);
+                curl_close($ch);
+
+                if ($datas) {
+                    $datas = json_decode($datas, true);
+
+                    if (is_array($datas) && $datas["country"] && $datas["country"] != "FR" && $datas["org"] && strpos(strtolower($datas["org"]), "google") === false) {
+                        $firewall_file = "../data/firewall/iptables.txt";
+                        $fs = new Filesystem();
+                        if ($fs->exists($firewall_file)) {
+                            if (strpos(file_get_contents($firewall_file), $this->getClientIp()) === false) {
+                                $fs->appendToFile($firewall_file, "iptables -I INPUT -s ".$this->getClientIp()." -j DROP\n");
+                                $drop_txt = "\n• *IP DROPPED* : " . $this->getClientIp();
+                            }
+                        } else {
+                            $fs->appendToFile($firewall_file, "iptables -I INPUT -s ".$this->getClientIp()." -j DROP\n");
+                        }
+                    }
+                }
+            }
+
             $data = array();
             $data['channel'] = '#errors';
             $data['username'] = $_SERVER['HTTP_HOST'];
@@ -63,6 +92,7 @@ class ExceptionListener
             $data['text'] .= "\n• *Erreur File* : " . strip_tags($event->getThrowable()->getFile()) . " at line :" . strip_tags($event->getThrowable()->getLine());
             $data['text'] .= "\n• *URL* : " . $event->getRequest()->getUri();
             $data['text'] .= "\n• *IP* : " . $this->getClientIp();
+            $data['text'] .= $drop_txt;
 
             if ($this->user) {
                 $data['text'] .= "\n• *Utilisateur* : " . $this->user . " with id : " . $this->user->getId();
